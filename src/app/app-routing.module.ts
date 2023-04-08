6c893561e48635f04da8fc66ea9f5ce93d853a2f
davidgalvez <davidgalvezfis@gmail.com>
2023-04-08 04:15:55 +0200

    Setting Routing Outlet and Adding pages to components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from '@modules/home/pages/home-page/home-page.component';
 
 const routes: Routes = [
   {
     path:'', //Localhost:42000/
+    component: HomePageComponent,
     loadChildren: () => import(`./modules/home/home.module`).then(m => m.HomeModule)
+  },
+  {
+    path:'auth', //Localhost:42000/
+    loadChildren: () => import(`./modules/auth/auth.module`).then(m => m.AuthModule)
   }
 ];
 
