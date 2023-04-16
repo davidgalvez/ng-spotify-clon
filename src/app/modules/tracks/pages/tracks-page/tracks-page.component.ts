361a8fa96f567287e6d4050215c9e1790ec5479f
davidgalvez <davidgalvezfis@gmail.com>
2023-04-16 18:28:39 +0200

    Including sorting and filtering pipes to track service

--- src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -17,13 +17,20 @@ export class TracksPageComponent implements OnInit, OnDestroy {
   constructor(private tracksService: TrackService){}
 
   ngOnInit():void {
-    const observer1$ =this.tracksService.getAllTracks$().subscribe(response=>{
-        this.tracksTrending=response.data
-        this.tracksRandom=response.data
+    const observer1$ =this.tracksService.getAllTracks$()
+    .subscribe((response:TrackModel[])=>{
+        this.tracksTrending=response
+      
         //console.log("Canciones trending:",response)
-      })   
+      })  
+      
+      const observer2$ =this.tracksService.getAllRandom$()
+    .subscribe((response:TrackModel[])=>{       
+        this.tracksRandom=response
+        //console.log("Canciones trending:",response)
+      })  
     
-    this.listObservers$ =[observer1$]
+    this.listObservers$ =[observer1$, observer2$]
   }
 
   ngOnDestroy(): void {
--- src/app/modules/tracks/services/track.service.ts
+++ src/app/modules/tracks/services/track.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
 import { Observable, of } from 'rxjs';
+import { map, mergeMap } from 'rxjs/operators'
 import * as dataRaw from '@data/tracks.json';
 import { Title } from '@angular/platform-browser';
 import { HttpClient } from '@angular/common/http';
@@ -19,7 +20,28 @@ export class TrackService {
   constructor(private httpClient:HttpClient) {     
   }
 
+  private skipById(listTracks:TrackModel[],id:number): Promise<TrackModel[]>{
+    return new Promise((resolve,reject)=>{
+      const listTemp=listTracks.filter(a=>a._id!==id)
+      resolve(listTemp)
+    })
+  }
+
   getAllTracks$():Observable<any>{
     return this.httpClient.get(`${this.URL}/tracks`)
+    .pipe(
+      map(({data}:any)=>{
+        return data
+      })
+    )
   }
+
+  getAllRandom$():Observable<any>{
+    return this.httpClient.get(`${this.URL}/tracks`)
+    .pipe(
+      mergeMap(({data}:any)=>this.skipById(data,2))
+    )
+  }
+
+
 }
