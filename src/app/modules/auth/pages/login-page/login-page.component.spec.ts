99dffd384431f05685d403b396c29edf86411cdc
davidgalvez <davidgalvezfis@gmail.com>
2023-04-26 19:59:35 +0200

    Implementing login form unit testing validations

--- src/app/modules/auth/pages/login-page/login-page.component.spec.ts
+++ src/app/modules/auth/pages/login-page/login-page.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import {HttpClientTestingModule} from '@angular/common/http/testing'
 import { LoginPageComponent } from './login-page.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
 
 describe('LoginPageComponent', () => {
   let component: LoginPageComponent;
@@ -22,4 +23,35 @@ describe('LoginPageComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should return invalid form', () => {
+    //Arrange
+    const mockCredentials={
+      email: 'email-erroneo.com',
+      password:'444444444555555555555444444444'
+    }
+
+    const emailForm:any=component.formLogin.get('email')
+    const passwordForm:any=component.formLogin.get('password')
+
+    //Act
+    emailForm.setValue(mockCredentials.email)
+    emailForm.setValue(mockCredentials.password)
+
+    //Assert
+    expect(component.formLogin.invalid).toEqual(true)
+
+
+  });
+
+  it('Button should show text "Iniciar sesión"',()=>{
+    //Arrange
+    const elementRef = fixture.debugElement.query(By.css('.form-action button'))    
+
+    //Act
+    const getInnerText =elementRef.nativeElement.innerText
+
+    //Assert
+    expect(getInnerText).toEqual('Iniciar sesión')
+  })
 });
